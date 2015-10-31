2b746e739630dabec1be86a769dbb57724a40649
Ramadhan Rosihadi Perdana <ramadhanrperdana@gmail.com>
2015-10-31 23:29:38 +0700

    Basic Design

--- application/views/surveykapal/keterangan_kapal.php
+++ application/views/surveykapal/keterangan_kapal.php
@@ -4,15 +4,88 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Keterangan kapal</title>
-
-  <!-- CSS  -->
-  <link href="<?php echo base_url(); ?>assets/css/materialize/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="<?php echo base_url(); ?>assets/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <title>Survey kapal</title>
+  <!-- css -->
+    <link href="<?php echo base_url(); ?>assets/blocks/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/blocks/css/main.css" rel="stylesheet">
+    <!-- DATA TABLE CSS -->
+    <link href="<?php echo base_url(); ?>assets/blocks/css/table.css" rel="stylesheet">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 </head>
 
-<h1>Keterangan Kapal</h1>
 <!-- Semangat!  -->
+
+<body>
+  	<div class="container">
+
+      <!-- CONTENT -->
+	<div class="row">
+		<div class="col-sm-12 col-lg-12">
+			<div class="judul-konten">
+				<p>Halaman keterangan Kapal </p>
+			</div>
+			  <table class="display">
+	          <thead>
+	            <tr>
+	              <th>Band or Group</th>
+	              <th>Name</th>
+	              <th>Instrument</th>
+	              <th>Influence</th>
+	              <th>Awesome</th>
+	              <th>Awesome</th>
+	              <th>Awesome</th>
+	            </tr>
+	          </thead>
+	          <tbody>
+	            <tr class="odd">
+	              <td>Queen</td>
+	              <td>Brian May</td>
+	              <td>Guitar</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	            </tr>
+	            <tr class="even">
+	              <td>Queen</td>
+	              <td>Roger Taylor</td>
+	              <td>Drums</td>
+	              <td class="center">5</td>
+	              <td class="center">7</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	            </tr>
+	            <tr class="odd">
+	              <td>Beatles</td>
+	              <td>Paul McCartney</td>
+	              <td>Guitar &amp; Piano</td>
+	              <td class="center">8</td>
+	              <td class="center">9</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	            </tr>
+	            <tr class="even">
+	              <td>Adele</td>
+	              <td>Adele</td>
+	              <td>None</td>
+	              <td class="center">8</td>
+	              <td class="center">8</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	            </tr>
+	            <tr class="odd">
+	              <td>Britney Spears</td>
+	              <td>Britney Spears</td>
+	              <td>None</td>
+	              <td class="center">7</td>
+	              <td class="center">-5</td>
+	              <td class="center"> 7</td>
+	              <td class="center">9</td>
+	            </tr>
+	          </tbody>
+	         </table><!--/END First Table -->
+			 <br>
+</body>
 
 
 </html>
--- application/views/surveykapal/survey_kapal_home.php
+++ application/views/surveykapal/survey_kapal_home.php
@@ -5,14 +5,61 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
   <title>Survey kapal</title>
-
   <!-- CSS  -->
   <link href="<?php echo base_url(); ?>assets/css/materialize/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="<?php echo base_url(); ?>assets/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- css -->
+    <link href="<?php echo base_url(); ?>assets/mamba/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="<?php echo base_url(); ?>assets/mamba/css/style.css" rel="stylesheet" media="screen">
+	<link href="<?php echo base_url(); ?>assets/mamba/color/default.css" rel="stylesheet" media="screen">
+	<script src="<?php echo base_url(); ?>assets/mamba/js/modernizr.custom.js"></script>
 </head>
 
-
 <!-- Semangat!  -->
+
+<body>
+  	<div id="intro">
+			<div class="intro-text">
+			<div class="upper-intro">
+				<p>Tugas Akhir Hakara Warid</p>
+				<p>Deskripsi 1</p>
+				<p>Deskripsi 2</p>
+			</div>
+				<div class="container">
+					<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-6">
+							<div class="brand">
+								<h1><a href="index.html">Menu 1</a></h1>
+								<div class="line-spacer"></div>
+								<p><span>Deskripsi menu</span></p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="brand">
+								<h1><a href="<?php echo base_url(); ?>home/keterangan_kapal">Menu 2</a></h1>
+								<div class="line-spacer"></div>
+								<p><span>Deskripsi menu</span></p>
+							</div>
+						</div>
+						<div class="bottom-intro">
+							<p>Kata-kata dibawahnya apa ini</p>
+						</div>
+					</div>
+					</div>
+				</div>
+		 	</div>
+	 </div>
+	 <footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Tugas Akhir - Hakara Warid</p>
+				</div>
+			</div>		
+		</div>	
+	</footer>
+</body>
 
 
 </html>
