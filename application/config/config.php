8031cbbaceaa9b117226015ccbe9bdfdcffc3319
Ramadhan Rosihadi Perdana <ramadhanrperdana@gmail.com>
2015-12-07 23:59:40 +0700

    .

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@
 //$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 //$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 //$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url']	= 'http://localhost/surveykapal/';
+$config['base_url']	= 'http://localhost:8080/surveykapal/';
 /*
 |--------------------------------------------------------------------------
 | Index File
